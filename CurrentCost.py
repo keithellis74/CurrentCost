301c0cad04f416328ebfdfbe585d9e4f465bd922
Keith <keith.ellis@mac.com>
2020-11-28 22:41:52 +0000

    Intergrating mqtt, part way through, not sure if it is getting through to the broker

--- CurrentCost.py
+++ CurrentCost.py
@@ -1,19 +1,16 @@
 import serial
 import xml.etree.ElementTree as ET
 import datetime
+import time
+import paho.mqtt.client as mqtt 
 
-'''
-remoteMQTTuser = "ptbw2000"
-remoteMQTTpassword = "!!! CHANGE TO YOUR ADAFRUIT.IO KEY. REDACT BEFORE PUSHING !!!"
-remoteMQTTtopic = "PVPower"
-remoteMQTTtopic2 = "Temperature"
+mqttBroker ="192.168.54.30" 
+mqttPort = 1883
+mqttUser = 'kellis'
+mqttPassword = 'cul8erm8'
 
-# You should not need to change anything below this line.
-
-remoteMQTTbroker = "io.adafruit.com"
-remoteMQTTport = 1883
-'''
-
+client = mqtt.Client(client_id="CurrentCost")
+client.connect(mqttBroker) 
 
 
 ser = serial.Serial(port='/dev/tty.usbserial-DN03ZPK9',
@@ -26,44 +23,65 @@ ser = serial.Serial(port='/dev/tty.usbserial-DN03ZPK9',
 ser.close()
 ser.open()
 
-keepLooping = True
-inputBuffer = ""
 
-while(keepLooping):
-	
-	try:
-		if ser.in_waiting > 0:
-			x = ser.read(ser.in_waiting)
-			inputBuffer = inputBuffer + str(x, 'utf-8')
-			
-			if inputBuffer.find("</msg>") > 0:
-				tempbuf = inputBuffer.split("</msg>", 1)
-				inputBuffer = tempbuf[1]
-				tempbuf[0] = tempbuf[0]+"</msg>"
+
+
+def getStream():
+	keepLooping = True
+	inputBuffer = ""
+	receiving = True
+	while receiving == True and keepLooping == True:
+		try:
+			if ser.in_waiting > 0:
+				x = ser.read(ser.in_waiting)
+				inputBuffer = inputBuffer + str(x, 'utf-8')
 				
-				root = ET.fromstring(tempbuf[0])
-
-				temp = 0;
-				watts = 0;
-
-				for child in root:
-					if child.tag == "tmpr" :
-						temp = float(child.text)
-					if child.tag == "ch1" :
-						watts = int(child.find('watts').text)
-						
-				if watts > 0 :		
-					print("Temperature: ", temp)
-					print("Power: ", watts)									
+				if inputBuffer.find("</msg>") > 0:
+					tempbuf = inputBuffer.split("</msg>", 1)
+					inputBuffer = tempbuf[1]
+					tempbuf[0] = tempbuf[0]+"</msg>"
 					
+					root = ET.fromstring(tempbuf[0])
 
-	except serial.SerialTimeoutException:
-		print ("TimeOut Error")
-	except serial.SerialException:
-		print ("serial Error")
+					temp = 0
+					watts = 0
+
+					for child in root:
+						if child.tag == "tmpr" :
+							temp = float(child.text)
+						if child.tag == "ch1" :
+							watts = int(child.find('watts').text)
+							
+					if watts > 0 :	
+						receiving = False
+						return temp, watts	
+						#print("Temperature: ", temp)
+						#print("Power: ", watts)									
+						
+
+		except serial.SerialTimeoutException:
+			print ("TimeOut Error")
+		except serial.SerialException:
+			print ("serial Error")
+		except KeyboardInterrupt:
+			print ("Closing down")
+			keepLooping = False
+
+looping = True
+while(looping):
+	try:
+		temp, watts = getStream()
+		print("Temperature = ", temp)
+		print("Power = ", watts)
+		time.sleep(0.5)
+		client.publish("TEMPERATURE", temp)
+		client.publish("POWER", watts)
+
+
 	except KeyboardInterrupt:
-		print ("Closing down")
-		keepLooping = False
+			print ("Closing down")
+			looping = False
+	
 
 # Close down connection. A better way to do this would be to create
 # a class to hold the connection objects, which would allow us to leave
